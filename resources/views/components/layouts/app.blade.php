50b28a282a9950c61e8f256ec169f885d1daa984
Rizqi Hendra Ardiansyah <Hendrasyahardi123@gmail.com>
2025-07-18 14:15:03 +0700

    update revisi

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>{{ $title ?? 'Smart Class' }}</title>
+     <link rel="icon" type="image/x-icon" href="{{ asset('storage/gambar/logo.png') }}">
     @vite('resources/css/app.css')
     @fluxStyles
 </head>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Smart</title>
+        <title>Smart Class</title>
 
          <link rel="icon" type="image/x-icon" href="{{ asset('storage/gambar/logo.png') }}">
         <!-- Fonts -->
